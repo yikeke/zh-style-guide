7a17fe61d53fc242ad7de939522edf2a33baa996
yikeke <yikeke@pingcap.com>
2020-10-15 17:29:46 +0800

    test custom css config

--- source/conf.py
+++ source/conf.py
@@ -117,3 +117,6 @@ html_sidebars = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Use custom CSS configs
+templates_path = ['_templates']
