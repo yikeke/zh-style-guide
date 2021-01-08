4617ea77cb9fc80f9dac0c042e6389396b28cc74
yikeke <yikeke@pingcap.com>
2021-01-08 15:11:10 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -23,7 +23,7 @@ project = '中文技术文档写作风格指南'
 # currentYear = datetime.date.year
 currentYear = datetime.now().year
 author = 'yikeke'
-copyright = currentYear + ', ' + author
+copyright = str(currentYear) + ', ' + author
 
 # The full version, including alpha/beta/rc tags
 release = '0.1'
